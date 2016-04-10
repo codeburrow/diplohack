d8dded0c79c5cff82d0f77327ac17467aeef7594
Antony <anthonykalogeropoulos@gmail.com>
2016-04-10 14:02:29 +0300

    Modify Search.php

--- app/Controllers/WelcomeController.php
+++ app/Controllers/WelcomeController.php
@@ -4,18 +4,40 @@
  * @since 4/7/16
  */
 namespace App\Controllers;
+use App\Manipulators\FundManipulator;
+use App\Services\FundsService;
+use App\Transformers\ApiFundTransformer;
+
 
 /**
  * Class WelcomeController.
  */
 class WelcomeController extends Controller
 {
+    protected  $fundsService;
+    private $fundsTransformer;
+    private $fundManipulator;
+
+    public function __construct(){
+
+	    parent::__construct();
+        $this->fundsService = new FundsService();
+        $this->fundsTransformer = new ApiFundTransformer();
+        $this->fundManipulator = new FundManipulator();
+    }
+
     /**
      * @return string
      */
     public function search()
     {
-        return $this->twig->render('search.twig', ['name' => 'Fabien']);
+        $funds = $this->fundsService->get();
+
+        $funds = $this->fundManipulator->concatenateLinks($funds);
+
+        $results = $this->fundsTransformer->transformCollection($funds);
+
+        return $this->twig->render('search.twig', ['results' => $results]);
     }
 
     public function welcome()
