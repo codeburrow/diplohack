8883eedc4111fd5b7146160f4dcf74bf83a19e16
Rizart Dokollari <r.dokollari@gmail.com>
2016-04-14 05:04:06 +0000

    Import data from excel to php variables.

--- database/commands/importExcel.php
+++ database/commands/importExcel.php
@@ -6,6 +6,30 @@
 require __DIR__.'/../../vendor/autoload.php';
 require __DIR__.'/../../app/bootstrap.php';
 
-$databaseLocation = __DIR__.'/../../storage/data_set.xlsx';
+require __DIR__.'/provision.php';
+
+$excelFileLocation = __DIR__.'/../../storage/data_set.xlsx';
+
+$objPHPExcel = PHPExcel_IOFactory::load($excelFileLocation);
+$sheet = $objPHPExcel->getSheet(0);
+$highestRow = $sheet->getHighestRow();
+$highestColumn = $sheet->getHighestColumn();
+
+for ($row = 2; $row <= $highestRow; $row++) {
+    $rowData = $sheet->rangeToArray('A'.$row.':'.$highestColumn.$row)[0];
+    
+    $categoryName = $rowData[0];
+    
+    var_dump(trim($categoryName));exit;
+}
+
+var_dump($objPHPExcel);
+exit;
+
+
+
+
+
+
 
 
